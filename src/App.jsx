a55f59306fa3401859afa069f607b92112cb79db
Jay Mendez <johnjosephmendez2016@gmail.com>
2020-04-04 16:23:41 +0800

    initial commit: boilerplate with eslint

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+import Drawer from "./components/layouts/Drawer";
+
+function App() {
+  return (
+    <div className="App">
+      <Drawer />
+    </div>
+  );
+}
+
+export default App;
